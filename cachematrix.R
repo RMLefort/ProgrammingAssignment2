01a40f1e6483299a115029b76821a35b46cdacda
rmlefort <rmlefort@msn.com>
2019-03-31 16:48:54 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -39,3 +39,50 @@ cacheSolve <- function(x, ...) {
   x$setinverse(inv)
   inv
 }
+
+##Tests
+> TestsMatrix <- matrix(1:4,2,2)
+> Cache <- makeCacheMatrix(TestsMatrix)
+> Cache$get()
+[,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> Cache$getinverse()
+NULL
+> cacheSolve(Cache)
+[,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+
+> TestsMatrix <- matrix(c(2,3,5,1,3,7,4,5,6,8,0,0,4,5,6,0),4,4)
+> Cache <- makeCacheMatrix(TestsMatrix)
+> Cache$get()
+[,1] [,2] [,3] [,4]
+[1,]    2    3    6    4
+[2,]    3    7    8    5
+[3,]    5    4    0    6
+[4,]    1    5    0    0
+> Cache$getinverse()
+NULL
+> cacheSolve(Cache)
+[,1] [,2]       [,3]       [,4]
+[1,]  40.0  -30 -1.6666667  19.333333
+[2,]  -8.0    6  0.3333333  -3.666667
+[3,]   9.5   -7 -0.5000000   4.500000
+[4,] -28.0   21  1.3333333 -13.666667
+
+
+> TestsMatrix <- matrix(c(2, 2, 1, 4), 2, 2)
+> Cache <- makeCacheMatrix(TestsMatrix)
+> Cache$get()
+[,1] [,2]
+[1,]    2    1
+[2,]    2    4
+> Cache$getinverse()
+NULL
+> cacheSolve(Cache)
+[,1]       [,2]
+[1,]  0.6666667 -0.1666667
+[2,] -0.3333333  0.3333333
+
+
